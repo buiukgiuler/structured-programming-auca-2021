5d85ec1885dd847aeaabc7ce10b58f4c78678fcf
Mustafakemal <Buiukgiuler_m@auca.kg>
2021-10-08 11:11:15 +0600

    lab-01 Problem03

---
+++ lab-01/src/Problem03.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+ class Problem03 {
+  public static void main(String[] args) {
+   System.out.print("What is your name?");
+
+   Scanner inp = new Scanner(System.in);
+   String userName = inp.nextLine();
+
+//  System.out.print("Hello);
+//  System.out.print("userName);
+//  System.out.print("!);
+
+   System.out.println("Hello, "+ userName + "!");
+
+  }
+ }
