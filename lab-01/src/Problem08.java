6c0b047d136e9d96733a83acdb897ae641e9bc4d
Mustafakemal <Buiukgiuler_m@auca.kg>
2021-10-08 12:49:55 +0600

    lab-01 Problem08

---
+++ lab-01/src/Problem08.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Problem08 {
+    public static void main(String[] args) {
+
+        int n  = 9;
+        int sum = 0;
+
+        sum = n*(n+1)/2;
+
+        System.out.println("Sum = " + sum);
+    }
+
+}
