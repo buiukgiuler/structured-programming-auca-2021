eb309521687f23646869a313fbbb8ff97625c7d7
Mustafakemal <Buiukgiuler_m@auca.kg>
2021-10-08 12:50:38 +0600

    lab-01 Problem09

---
+++ lab-01/src/Problem09.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Problem09 {
+    public static void main(String[] args) {
+
+        double n  = 1.0;
+        double sum = 0;
+        double sum1 = 0;
+
+        sum =4*(1-n/3+n/5-n/7+n/9-n/11);
+        sum1 =4*(1-n/3+n/5-n/7+n/9-n/11+n/13);
+
+        System.out.println("Sum = " + sum);
+        System.out.println("Sum = " + sum1);
+
+
+    }
+
+}
