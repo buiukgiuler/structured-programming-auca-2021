f213a35e1b908d3e6f13d925aa7c204a377eea76
Mustafakemal <Buiukgiuler_m@auca.kg>
2021-10-10 01:25:36 +0600

    Lab01 Problem07

---
+++ lab-01/src/Problem07.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Problem04 {
+
+    public static void main(String args[]) {
+        int a = 1;
+        int b = 4;
+
+        for (int i = a; i <= b; i++) {
+            System.out.printf( i + "^2 = %.0f \n", Math.pow(i, 2));
+
+
+
+        }
+    }
+}
+
