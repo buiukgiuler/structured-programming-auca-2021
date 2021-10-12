3fb597eb078f500a3c73f3814a84ac6677054282
Mustafakemal <Buiukgiuler_m@auca.kg>
2021-10-12 22:35:20 +0600

    lab-02 Problem07

---
+++ lab-02/src/Problem07.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Problem07 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print(" Enter the radius value");
+        double radius = in.nextDouble();
+
+        System.out.print(" Enter the length value");
+        double length = in.nextDouble();
+
+        double area = (radius * radius * Math.PI);
+        double volume = (area * length);
+
+        System.out.printf("the area is " + "%.2f", area);
+
+        System.out.println();
+
+        System.out.printf("the volume is " + "%.2f", volume);
+
+    }
+}
