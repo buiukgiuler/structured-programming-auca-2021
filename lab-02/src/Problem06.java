875cb2c2f5f030a1448cac7bda69556e7c52ae3e
Mustafakemal <Buiukgiuler_m@auca.kg>
2021-10-12 22:00:44 +0600

    lab-02 Problem06

---
+++ lab-02/src/Problem06.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Problem06 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Enter a degree in Celsius: ");
+
+        double celsius = in.nextDouble();
+
+        double farenheit = (celsius * 9.0/5) + 32;
+
+        System.out.printf( celsius + " Celsius is " + "%.2f farenheit" , farenheit);
+
+    }
+}
