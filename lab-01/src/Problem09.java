8f43f1b08f31d48867e33316245d039514a52997
Mustafakemal <Buiukgiuler_m@auca.kg>
2021-10-12 21:29:18 +0600

    lab-02 Problem01

--- lab-01/src/Problem09.java
+++ lab-01/src/Problem09.java
@@ -4,8 +4,8 @@ public class Problem09 {
     public static void main(String[] args) {
 
         double n  = 1.0;
-        double sum = 0;
-        double sum1 = 0;
+        double sum;
+        double sum1;
 
         sum =4*(1-n/3+n/5-n/7+n/9-n/11);
         sum1 =4*(1-n/3+n/5-n/7+n/9-n/11+n/13);
