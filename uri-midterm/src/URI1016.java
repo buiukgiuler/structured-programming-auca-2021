725c47e02be00239c2b411b3dd3fc27c614f97f3
Mustafakemal <Buiukgiuler_m@auca.kg>
2021-10-23 12:51:45 +0600

    URI1016 accepted

---
+++ uri-midterm/src/URI1016.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class URI1016 {
+    public static void main(String[] args) {
+        Scanner sc  = new Scanner(System.in);
+
+        int  U  = sc.nextInt();
+        int  T  = U * 2;
+
+        System.out.println(T +  " minutos");
+
+
+    }
+
+}
