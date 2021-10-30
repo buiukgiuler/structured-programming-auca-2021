6d17446159743a58a31d3bfd4da7e590e3b874d4
Mustafakemal <Buiukgiuler_m@auca.kg>
2021-10-30 13:12:17 +0600

    URI1013 accepted

---
+++ uri-midterm/src/URI1013.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class URI1013 {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        int ab = (a + b + Math.abs(a - b))/2;
+        int abc = (ab + c + Math.abs(ab - c))/2;
+
+        System.out.println(abc + " eh o maior");
+    }
+}
