fb10e2ed955a39047819cdd527e35f1459f2b6d3
Mustafakemal <Buiukgiuler_m@auca.kg>
2021-11-09 22:05:58 +0600

    lab-04 Problem03 completed

---
+++ lab-04/src/Problem03.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class Problem03 {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+
+        System.out.print("Number of points?");
+        int mounth = inp.nextInt();
+
+        switch (mounth) {
+            case 12:
+            case 1:
+            case 2:
+                System.out.println("Winter");
+                break;
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("Spring");
+                break;
+            case 6:
+            case 7:
+            case 8:
+                System.out.println("Summer");
+                break;
+            default:
+                System.out.println("Fall");
+                break;
+        }
+    }
+}
