86c0252c93e3b7c3bff3cc23a5c7f028f8247829
Mustafakemal <Buiukgiuler_m@auca.kg>
2021-11-23 23:31:06 +0600

    lab-05 Problem03 completed

---
+++ lab-05/src/Problem03.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Problem03 {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+
+        Random rnd = new Random();
+
+        System.out.print("Number of testa?");
+        int nTests = inp.nextInt();
+
+        int i = 0;
+        while (i < nTests) {
+            System.out.println(-50 + rnd.nextInt(101));
+            ++i;
+        }
+    }
+}
