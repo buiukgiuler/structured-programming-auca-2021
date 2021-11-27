7c6de5ed02c2dbab9181bd73750a1802d04bbc43
Mustafakemal <Buiukgiuler_m@auca.kg>
2021-11-27 20:26:36 +0600

    URI1095 accepted

---
+++ uri-final/src/URI1095.java
@@ -0,0 +1,11 @@
+public class URI1095 {
+
+    public static void main(String[] args) {
+
+        for (int I = 1, J = 60; J >= 0; I += 3, J -= 5) {
+            System.out.print("I = " + I + " J = " + J + "\n");
+        }
+
+    }
+
+}
