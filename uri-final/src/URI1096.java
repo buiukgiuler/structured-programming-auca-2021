f78cebd3ff386ccd3b62582eb5fe746421b45368
Mustafakemal <Buiukgiuler_m@auca.kg>
2021-11-27 20:35:47 +0600

    URI1096 accepted

---
+++ uri-final/src/URI1096.java
@@ -0,0 +1,13 @@
+public class URI1096 {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 9; i += 2) {
+            for (int I = i,J = 7; (J >= 5); J-=1) {
+                System.out.print("I=" + I + " J ="+J+"\n");
+
+            }
+        }
+    }
+
+}
