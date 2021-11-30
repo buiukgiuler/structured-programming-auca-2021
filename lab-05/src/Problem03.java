7566ef3bb48f4ae01e8957135dc90aee664fba29
Mustafakemal <Buiukgiuler_m@auca.kg>
2021-11-30 21:21:56 +0600

    lab-05 Problem03 completed

--- lab-05/src/Problem03.java
+++ lab-05/src/Problem03.java
@@ -8,10 +8,24 @@ public class Problem03 {
         System.out.print("Number of testa?");
         int nTests = inp.nextInt();
 
+        int numofCorrectanswers = 0;
+
         int i = 0;
         while (i < nTests) {
-            System.out.println(-50 + rnd.nextInt(101));
-            ++i;
+            int op1 = -50 + rnd.nextInt(101);
+            int op2 = -50 + rnd.nextInt(101);
+            System.out.printf("%d + %d = ", op1,op2);
+            int unitAnswer = inp.nextInt();
+            if (op1 + op2 == unitAnswer) {
+                ++numofCorrectanswers;
+            }
+        ++i;
         }
+
+        System.out.println("Number of correct answers: " + numofCorrectanswers);
+        System.out.println("Number of incorrect answers: " + (nTests - numofCorrectanswers));
+
     }
+
+
 }
