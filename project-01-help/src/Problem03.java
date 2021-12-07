1c6cdd127c96c8dd6d6856c4a5adf9e1813be6c4
Mustafakemal <Buiukgiuler_m@auca.kg>
2021-12-07 22:03:14 +0600

    Problem03

--- project-01-help/src/Problem03.java
+++ project-01-help/src/Problem03.java
@@ -1,8 +1,8 @@
 import processing.core.*;
 
 public class Problem03 extends PApplet {
-    static final float RECT_WIDHT = 100;
-    static final float RECT_HEIGHT = 650;
+    static final float RECT_WIDTH = 580;
+    static final float RECT_HEIGHT = 80;
 
     public void settings() {
         fullScreen();
@@ -11,56 +11,60 @@ public class Problem03 extends PApplet {
     public void setup() {
         textSize(40);
         textAlign(CENTER);
-        frameRate(40);
-
+        frameRate(30);
     }
 
     public void draw() {
         background(0, 0, 0);
 
-        float rectTopLeftX = width / 2.8f - RECT_WIDHT / 2.5f;
-        float rectTopLeftY = width / 4f - RECT_HEIGHT / 2f;
+        float rectX = width / 2f - RECT_WIDTH / 2f;
+        float rectY = height / 4.5f - RECT_HEIGHT / 2f;
 
-        float rectTopLeftM = width / 2.8f - RECT_WIDHT / 2.5f;
-        float rectTopLeftN = width / 4f - RECT_HEIGHT / 2.9f;
+        float rectA = width / 2f - RECT_WIDTH / 2f;
+        float rectB = height / 4.5f + RECT_HEIGHT / 2f;
 
-        float rectTopLeftA = width / 2.8f - RECT_WIDHT / 2.5f;
-        float rectTopLeftB = width / 4f - RECT_HEIGHT / 4.9f;
+        float rectM = width / 2f - RECT_WIDTH / 2f;
+        float rectN = height / 4.5f + 3 * RECT_HEIGHT / 2f;
 
-        fill(0, 0, 255);
-        stroke(0,255,0);
-        rect(rectTopLeftX, rectTopLeftY, RECT_HEIGHT, RECT_WIDHT);
+        stroke(0, 0, 255);
+        fill(0, 0, 205);
+        rect(rectX, rectY, RECT_WIDTH, RECT_HEIGHT);
+        fill(255, 255, 255);
+        text("Java Programming Language", rectX, rectY, RECT_WIDTH, RECT_HEIGHT);
 
-        fill(0, 0, 255);
-        stroke(0,255,0);
-        rect(rectTopLeftM, rectTopLeftN, RECT_HEIGHT, RECT_WIDHT);
+        stroke(0, 0, 255);
+        fill(0, 0, 205);
+        rect(rectA, rectB, RECT_WIDTH, RECT_HEIGHT);
+        fill(255, 255, 255);
+        text("Kotlin Programming Language",rectA, rectB, RECT_WIDTH, RECT_HEIGHT);
 
-        fill(0, 0, 255);
-        stroke(0,255,0);
-        rect(rectTopLeftA, rectTopLeftB, RECT_HEIGHT, RECT_WIDHT);
+        stroke(0, 0, 255);
+        fill(0, 0, 205);
+        rect(rectM, rectN, RECT_WIDTH, RECT_HEIGHT);
+        fill(255, 255, 255);
+        text("Scala Programming Language", rectM, rectN, RECT_WIDTH, RECT_HEIGHT);
 
-        fill(255);
-        textAlign(LEFT);
-        text("Java Programming Language " ,rectTopLeftX*2.1f/1.9f, rectTopLeftY*2.1f/1.5f);
-
-        fill(255);
-        textAlign(LEFT);
-        text("Kotlin Programming Language " ,rectTopLeftM*2.3f/2.1f, rectTopLeftN*2.1f/1.7f);
-
-        fill(255);
-        textAlign(LEFT);
-        text("Scala Programming Language " ,rectTopLeftA*2.1f/1.9f, rectTopLeftB*2.1f/1.8f);
-
-
-        if (rectTopLeftX <= mouseX && mouseX <= rectTopLeftX + RECT_WIDHT && rectTopLeftY <= mouseY && mouseY <= rectTopLeftY + RECT_HEIGHT) {
+        if (rectX <= mouseX && mouseX <= rectX + RECT_WIDTH && rectY <= mouseY && mouseY <= rectY + RECT_HEIGHT) {
             fill(255, 255, 0);
-            text("Year:2004.PYPL Index:20", width / 3f, height - height / 3f);
-
+            text("Year: 1995. PYPL Index: 2", width / 2f, height - height / 4f);
+            fill(255, 0, 0);
+            text("Java Programming Language", rectX, rectY, RECT_WIDTH, RECT_HEIGHT);
+        }
+        if (rectA <= mouseX && mouseX <= rectA + RECT_WIDTH && rectB <= mouseY && mouseY <= rectB + RECT_HEIGHT) {
+            fill(255, 255, 0);
+            text("Year: 2011. PYPL Index: 11", width / 2f, height - height / 4f);
+            fill(255, 0, 0);
+            text("Kotlin Programming Language", rectA,rectB, RECT_WIDTH, RECT_HEIGHT);
+        }
+        if (rectM <= mouseX && mouseX <= rectM + RECT_WIDTH && rectN <= mouseY && mouseY <= rectN + RECT_HEIGHT) {
+            fill(255, 255, 0);
+            text("Year: 2004. PYPL Index: 20", width / 2f, height - height / 4f);
+            fill(255, 0, 0);
+            text("Scala Programming Language", rectM, rectN, RECT_WIDTH, RECT_HEIGHT);
         }
     }
 
-    public static void main(String[] args) {
+    public static void main (String[]args){
         PApplet.main("Problem03");
     }
-
 }
