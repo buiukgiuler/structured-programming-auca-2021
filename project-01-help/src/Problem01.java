4098ba47a27377e3df6012441ed13475fb1859d0
Mustafakemal <Buiukgiuler_m@auca.kg>
2021-12-07 18:33:15 +0600

    Problem01

--- project-01-help/src/Problem01.java
+++ project-01-help/src/Problem01.java
@@ -2,11 +2,13 @@ import processing.core.*;
 
 public class Problem01 extends PApplet {
     static float MIN_SIZE = 10;
+    static float CHANGE = 2;
     static float MAX_SIZE = 144;
-
     static String MESSAGE = "Hello, Processing!!!";
 
+    float colorcount = 1;
     float messageSize;
+
 
     public void settings() {
         fullScreen();
@@ -18,19 +20,33 @@ public class Problem01 extends PApplet {
     }
 
     public void draw() {
+
         background(0, 0, 0);
         textSize(messageSize);
+        text(MESSAGE, width / 2f, height / 2f);
         textAlign(CENTER, CENTER);
-        fill(255, 0,0);
+        messageSize += CHANGE;
 
-        if (messageSize <= MAX_SIZE) {
-            messageSize += 1;
-            text("Hello, Processing!!!", width / 2f, height / 2f);
-        } else if (messageSize > MAX_SIZE) {
-            messageSize -= 144;
-            text("Hello, Processing!!!", width / 2f, height / 2f);
+        if (messageSize >= MAX_SIZE || messageSize <= MIN_SIZE) {
+            CHANGE = -CHANGE;
+        }
+        if (messageSize == MIN_SIZE) {
+            colorcount += 1;
+        }
+        if (colorcount == 1) {
+            fill(255, 0, 0);
+        }
+        if (colorcount == 2) {
+            fill(0, 255, 0);
+        }
+        if (colorcount == 3) {
+            fill(0, 0, 255);
+        }
+        if (colorcount == 4 && messageSize <= MIN_SIZE) {
+            colorcount = 1;
         }
     }
+
 
     public static void main(String[] args) {
         PApplet.main("Problem01");
