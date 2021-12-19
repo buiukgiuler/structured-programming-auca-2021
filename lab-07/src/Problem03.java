285437e7d4994bf849e8505c80e2dff79d0a7836
Mustafakemal <Buiukgiuler_m@auca.kg>
2021-12-19 20:38:32 +0600

    lab-07 Problem07 completed

---
+++ lab-07/src/Problem03.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Problem03 {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+
+        System.out.print("N: ");
+        int n = inp.nextInt();
+
+        int[] a = new int[n];
+
+        for (int i = 0; i < n; ++i) {
+            System.out.print("Enter element with index" + i + ": ");
+            a[i] = inp.nextInt();
+        }
+        printArray(a);
+        reverseArray(a);
+        printArray(a);
+    }
+
+    private static void reverseArray(int[] v) {
+        for (int left = 0, right = v.length - 1; left < right; ++left, --right) {
+
+            int t = v[left];
+            v[left] = v[right];
+            v[right] = t;
+        }
+
+    }
+
+    private static void printArray(int[] a) {
+        for (int i = 0; i < a.length; ++i) {
+            System.out.print(a[i] + "");
+
+        }
+        System.out.println();
+
+    }
+}
+
+
